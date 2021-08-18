2c84b893a56f0fa3cc8a3db2e6df9695c62246eb
Matus <matus.klement@superfaktura.sk>
2021-08-18 23:01:28 +0200

    Strip gulp and unnecessary packages for minimal working global dependencies bundle

--- build.js
+++ build.js
@@ -1,13 +1,52 @@
 const browserify = require('browserify')
-const babel = require('babelify');
 const glob = require('glob')
 
-glob('webroot/js/global_modules/*.js', {}, (err, files) => {
-    const browserifyInstance = browserify(files, {debug: true}).transform(
-        babel.configure({
-            presets: ['@babel/preset-env']
-        })
-    )
+/**
+ * Whitelist vendorov, ktore globalne nacitame
+ *
+ * @type {string[]}
+ */
+const globalVendorWhitelist = [
+    'lodash',
+]
 
-    browserifyInstance.bundle().pipe(process.stdout)
+/**
+ * Vytvori mapu globalnych balickov podla konvencie:
+ * js/global_modules/{FILE_NAME} -> require('{FILE_NAME}')
+ *
+ * @param {string[]} filenames
+ * @return {{file: string, expose: string}[]}
+ */
+function getRequireObject(filenames) {
+    return filenames.map(
+        filename => {
+            const splitPath = filename.split('/')
+            const baseName = splitPath[splitPath.length - 1].replace(/.m?js/, '')
+
+            return {file: filename, expose: baseName}
+        }
+    )
+}
+
+/**
+ * Zmapuj zoznam vendorov na pole sparsovatelne browserify.require
+ *
+ * @param packages
+ * @return {{file: string}[]}
+ */
+function getDepsArray(packages) {
+    return packages.map(dependency => ({file: dependency}))
+}
+
+glob('./webroot/js/global_modules/*.js', (err, files) => {
+    const requireEntries = getRequireObject(files)
+    const depPackages = getDepsArray(globalVendorWhitelist)
+
+    const browserifyInstance = browserify({entries: files}, {debug: true})
+        .transform('babelify')
+        .require([...requireEntries, ...depPackages])
+
+    browserifyInstance
+        .bundle()
+        .pipe(process.stdout, 'utf8')
 })
